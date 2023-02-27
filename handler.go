97b1c5c50312d84fb0d64e14f73df8b3837310c0
yaryna-bashchak <y.bashchak.im11@kpi.ua>
2023-02-27 19:32:21 +0200

    format code (go fmt .)

--- handler.go
+++ handler.go
@@ -1,23 +1,23 @@
 package main
 
 import (
-"encoding/json"
-"net/http"
-"time"
+	"encoding/json"
+	"net/http"
+	"time"
 )
 
 type TimeResponse struct {
 	Time string `json:"time"`
 }
 
-func Handler(w http.ResponseWriter, req *http.Request){
+func Handler(w http.ResponseWriter, req *http.Request) {
 	currentTime := time.Now()
 	response := TimeResponse{Time: currentTime.Format(time.RFC3339)}
 	jsonResponse, err := json.MarshalIndent(response, "", " ")
 
 	if err != nil {
 		http.Error(w, "Error creating JSON response", http.StatusInternalServerError)
-		return;
+		return
 	}
 
 	w.Header().Set("Content-Type", "application/json")
