1e15393d727c78ef9a9a273faf0dd9821a65ebb6
Hatrison <vadimulanovskiy@gmail.com>
2023-02-27 18:02:47 +0200

    Vadim Lab1 file edited

--- handler.go
+++ handler.go
@@ -11,5 +11,15 @@ type TimeResponse struct {
 }
 
 func Handler(w http.ResponseWriter, req *http.Request){
+	currentTime := time.Now()
+	response := TimeResponse(Time: currentTime.Format(time.RFC3339))
+	jsonResponse, err := json.MarshalIndent(response, "", " ")
 
+	if err != nil {
+		http.Error(w, "Error creating JSON response", http.StatusInternalServerError)
+		return;
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(jsonResponse)
 }
