1ff6574132611415a461c40cc88bf65db6578693
Tutinichic <da295615@gmail.com>
2023-04-29 16:10:25 +0300

    fix links to github

--- lab3/cmd/painter/main.go
+++ lab3/cmd/painter/main.go
@@ -3,9 +3,9 @@ package main
 import (
 	"net/http"
 
-	"github.com/roman-mazur/architecture-lab-3/painter"
-	"github.com/roman-mazur/architecture-lab-3/painter/lang"
-	"github.com/roman-mazur/architecture-lab-3/ui"
+	"github.com/yaryna-bashchak/kpi-architecture-labs/lab3/painter"
+	"github.com/yaryna-bashchak/kpi-architecture-labs/lab3/painter/lang"
+	"github.com/yaryna-bashchak/kpi-architecture-labs/lab3/ui"
 )
 
 func main() {
--- lab3/go.mod
+++ lab3/go.mod
@@ -1,4 +1,4 @@
-module github.com/roman-mazur/architecture-lab-3
+module github.com/yaryna-bashchak/kpi-architecture-labs/lab3
 
 go 1.20
 
