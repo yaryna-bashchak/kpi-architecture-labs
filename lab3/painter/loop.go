d3236f71911a02f3d1ead84b76999529414c7fb8
yaryna-bashchak <y.bashchak.im11@kpi.ua>
2023-06-12 17:22:52 +0300

    refactor loop implementation

--- lab3/painter/loop.go
+++ lab3/painter/loop.go
@@ -1,8 +1,10 @@
 package painter
 
 import (
-	"golang.org/x/exp/shiny/screen"
 	"image"
+	"sync"
+
+	"golang.org/x/exp/shiny/screen"
 )
 
 // Receiver отримує текстуру, яка була підготовлена в результаті виконання команд у циелі подій.
@@ -16,6 +18,9 @@ type Loop struct {
 	next     screen.Texture // текстура, яка зараз формується
 	prev     screen.Texture // текстура, яка була відправленя останнього разу у Receiver
 	MsgQueue MessageQueue
+	stopCh   chan struct{}
+	mutex    sync.Mutex
+	cond     *sync.Cond
 }
 
 var size = image.Pt(400, 400)
@@ -25,41 +30,76 @@ func (l *Loop) Start(s screen.Screen) {
 	l.next, _ = s.NewTexture(size)
 	l.prev, _ = s.NewTexture(size)
 	l.MsgQueue = MessageQueue{}
+	l.stopCh = make(chan struct{})
+	l.cond = sync.NewCond(&l.mutex)
 	go l.processEvents()
 }
+
 func (l *Loop) processEvents() {
 	for {
-		if op := l.MsgQueue.Pull(); op != nil {
-			if update := op.Do(l.next); update {
-				l.Receiver.Update(l.next)
-				l.next, l.prev = l.prev, l.next
+		select {
+		case <-l.stopCh:
+			return
+		default:
+			l.mutex.Lock()
+			if l.MsgQueue.IsEmpty() {
+				l.cond.Wait()
+			}
+			op := l.MsgQueue.Pull()
+			l.mutex.Unlock()
+
+			if op != nil {
+				if update := op.Do(l.next); update {
+					l.Receiver.Update(l.next)
+					l.next, l.prev = l.prev, l.next
+				}
 			}
 		}
 	}
 }
 
+func (mq *MessageQueue) IsEmpty() bool {
+	mq.mutex.Lock()
+	defer mq.mutex.Unlock()
+	return len(mq.Queue) == 0
+}
+
 // Post додає нову операцію у внутрішню чергу.
+
 func (l *Loop) Post(op Operation) {
 	if op != nil {
 		l.MsgQueue.Push(op)
+		l.mutex.Lock()
+		l.cond.Signal()
+		l.mutex.Unlock()
 	}
 }
 
 // StopAndWait сигналізує
 func (l *Loop) StopAndWait() {
+	l.stopCh <- struct{}{}
+	<-l.stopCh
 }
 
 type MessageQueue struct {
 	Queue []Operation
+	mutex sync.Mutex
 }
 
 func (mq *MessageQueue) Push(op Operation) {
+	mq.mutex.Lock()
+	defer mq.mutex.Unlock()
 	mq.Queue = append(mq.Queue, op)
 }
+
 func (mq *MessageQueue) Pull() Operation {
+	mq.mutex.Lock()
+	defer mq.mutex.Unlock()
+
 	if len(mq.Queue) == 0 {
 		return nil
 	}
+
 	op := mq.Queue[0]
 	mq.Queue = mq.Queue[1:]
 	return op
